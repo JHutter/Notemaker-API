92f1b8a2432642000b7bb023392b7419ec0162db
Jon Hutter <jon.elliot.hutter@gmail.com>
2017-06-10 15:25:38 -0700

    path params

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class RestPage(webapp2.RequestHandler):
 class ProfileIDPage(webapp2.RequestHandler):
     def get(self, profile_id):
         self.response.write('Welcome...')
-        self.response.write('Profile ID')
+        self.response.write('Profile ID: ', profile_id)
         
 class ProfileListPage(webapp2.RequestHandler):
     def get(self):
