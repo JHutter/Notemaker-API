89da8f045a8c4d53294b54e9423cadfffa61a882
Jon Hutter <jon.elliot.hutter@gmail.com>
2017-06-10 16:04:54 -0700

    get returns json

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class ProfileIDPage(webapp2.RequestHandler):
           'payload': 'some var',
         } 
         self.response.status_int(403)
-        self.response.write(json.dumps(obj))
+        self.response.out.write(json.dumps(obj))
         
 class ProfileListPage(webapp2.RequestHandler):
     def get(self):
