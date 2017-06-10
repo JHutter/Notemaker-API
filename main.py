ebf16f603c62b0e270a26ccf0d51a800c923857a
Jon Hutter <jon.elliot.hutter@gmail.com>
2017-06-10 13:22:23 -0700

    path params

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ oauth_redir = 'https://final-project-496-400.appspot.com/oauth'
 class RestPage(webapp2.RequestHandler):
     def get(self):
         self.response.write('Welcome...')
-        self.response.out.write('Profile ID, ' + profile_id)
         
 
 class ProfileIDPage(webapp2.RequestHandler):
