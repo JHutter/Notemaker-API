e03dcefb698e15a89bb040960cda8ca9b276fa62
Jon Hutter <jon.elliot.hutter@gmail.com>
2017-06-11 02:09:25 -0700

    get profiles by id

--- main.py
+++ main.py
@@ -87,13 +87,8 @@ class ProfileIDPage(webapp2.RequestHandler):
         query = Profile.query(Profile.userid == profile_id)
         if (query.get() is None):
             self.response.out.write(json.dumps({'profiles':[]}))
-        elif (auth):
-            queryF = Profile.query(Profile.userid == profile_id).fetch()
-            self.response.out.write(json.dumps({'profiles':[line.to_dict() for line in queryF]}))
         else: 
-            queryF = Profile.query(Profile.userid == profile_id).fetch(projection=[Profile.handle, Profile.feeling, Profile.bio])
-            self.response.out.write(json.dumps({'profiles':[line.to_dict() for line in queryF]}))
-            #self.response.out.write(json.dumps({'profiles':[line.to_dict() for line in Profile.query(Profile.userid == profile_id).fetch(projection=[Profile.handle, Profile.feeling, Profile.bio])]}))
+            self.response.out.write(json.dumps({'profiles':[line.to_dict() for line in Profile.query(Profile.userid == profile_id).fetch(projection=[Profile.handle, Profile.feeling, Profile.bio])]}))
      
 # GET: all profiles
 # POST:      
