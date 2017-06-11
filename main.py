4c13170092d2ac4069aa7598d28aca86a3c58721
Jon Hutter <jon.elliot.hutter@gmail.com>
2017-06-10 23:08:18 -0700

    post profile

--- main.py
+++ main.py
@@ -100,7 +100,8 @@ class ProfileListPage(webapp2.RequestHandler):
             feeling = self.request.get('feeling', default_value=' ')
             bio = self.request.get('bio', default_value='somebody')
             
-            if (Profile.query(Profile.userid == user_id)): # trying to keep a uniqueness constraint here, even tho ndb doesn't support them
+            query = Profile.query(Profile.userid == user_id)
+            if (query.has_next()): # trying to keep a uniqueness constraint here, even tho ndb doesn't support them
                 #no don't add
                 self.response.write('user already exists')
                 status = '409 Conflict'
